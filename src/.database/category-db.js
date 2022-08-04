9cb319aa98408e0b47d1c01ef01e7456ca6c14f2
Marcello Thiry <marcello.thiry@gmail.com>
2022-08-04 09:44:21 -0300

    feat(all): Initial setup

---
+++ src/.database/category-db.js
@@ -0,0 +1,20 @@
+export const categoryDB = [
+    {
+        "id": "spaces",
+        "title": "Groups",
+        "c_node_gradient_upper": null,
+        "c_node_gradient_lower": null
+    },
+    {
+        "id": "fitness",
+        "title": "Movement & Fitness",
+        "c_node_gradient_upper": "ECC7E5",
+        "c_node_gradient_lower": "9EC6EB",
+    },
+    {
+        "id": "womenshealth",
+        "title": "Women's Health",
+        "c_node_gradient_upper": "ECC7E5",
+        "c_node_gradient_lower": "9EC6EB"
+    }
+]
