3e2ff02c95091b111b191269a3aa80985933e570
Marcello Thiry <marcello.thiry@gmail.com>
2022-08-04 10:51:19 -0300

    feat(db): Test suite for the category repository (database mockup)

--- src/repositories/category-repository.js
+++ src/repositories/category-repository.js
@@ -1,3 +1,38 @@
 export default class CategoryRepository {
 
+    /**
+     * @return an array with all categories from the database
+     */
+    static findAll() {
+        return []
+    }
+
+    /**
+     * @return the category if there is an entity with the provided ID, otherwise undefined
+     */
+    static findById(id) {
+        return undefined
+    }
+
+    /**
+     * Create or update the category provided. Usually, I return the new entity with the ID
+     * (automatically generated when creating). If the entity already has an ID, then it's an
+     * update.
+     * @param category the category to be saved
+     * @return the saved category
+     */
+    static save(category) {
+        return undefined
+    }
+
+    /**
+     * Remove the category provided. If there is no entity for the provided ID, it throws
+     * an error.
+     * @param id the category's ID to be removed
+     * @return the removed category
+     */
+    static remove(id) {
+        return undefined
+    }
+
 }
---
+++ src/repositories/category-repository.test.js
@@ -0,0 +1,64 @@
+import {expect} from 'chai'
+import CategoryRepository from './category-repository.js'
+
+describe('Category repository test suite', () => {
+    it('should return all 3 categories ', () => {
+        const categories = CategoryRepository.findAll()
+        expect(categories).to.be.a('array')
+        expect(categories).to.have.lengthOf(3)
+        expect(categories[0].id).to.equal('spaces')
+        expect(categories[1].id).to.equal('fitness')
+        expect(categories[2].id).to.equal('womenshealth')
+    })
+
+    it('should return a specific category (by its id - fitness)', () => {
+        const category = CategoryRepository.findById('fitness')
+        expect(category).to.not.be.a('array')
+        expect(category.title).to.equal('Movement & Fitness')
+    })
+
+    it('should not find a non existent category - kids', () => {
+        const category = CategoryRepository.findById('kids')
+        expect(category).to.equal(undefined)
+    })
+
+    it('should add the category kids', () => {
+        const kids = {
+            id: 'kids',
+            title: 'Kids Wear',
+            c_node_gradient_upper: 'AB1234',
+            c_node_gradient_lower: 'CD4321',
+        }
+        CategoryRepository.save(kids)
+        const category = CategoryRepository.findById('kids')
+        expect(category).to.equal(kids)
+    })
+
+    it('should update category kids', () => {
+        const newCategory = CategoryRepository.findById('kids')
+        newCategory.title = 'Kids Wear & Toys'
+        const saved = CategoryRepository.save(newCategory)
+        const expected = {
+            id: 'kids',
+            title: 'Kids Wear & Toys',
+            c_node_gradient_upper: 'AB1234',
+            c_node_gradient_lower: 'CD4321',
+        }
+        expect(saved).to.equal(expected)
+        const category = CategoryRepository.findById('kids')
+        expect(category).to.equal(expected)
+    })
+
+    it('should remove the category kids', () => {
+        const category = CategoryRepository.remove('kids')
+        expect(category).to.not.be.a('array')
+        expect(category.title).to.equal('Kids Wear & Toys')
+        const removed = CategoryRepository.findById('kids')
+        expect(removed).to.equal(undefined)
+    })
+
+    it('should return an error when trying to remove the non existent category kids', () => {
+        expect(() => CategoryRepository.remove('kids')).to.throw('User not found for id "kids"')
+    })
+
+})
